f0698c969a4d69beebcd1c20e53bc771d12dd3aa
Blaž Škufca <3877198+blazskufca@users.noreply.github.com>
2025-03-31 18:21:32 +0200

    [__init__.py] - Fix up exports

--- tcp_to_http/__init__.py
+++ tcp_to_http/__init__.py
@@ -4,13 +4,15 @@ TCP to HTTP - A simple Golang style HTTP server implementation in Python.
 
 __version__ = "0.0.1"
 
+from .headers import Headers
 from .logger import get_logger, logger
 from .request import Request
 from .response_writer import Writer
 from .responses import StatusCode, get_default_headers
-from .server import Server, serve
+from .server import Server
 
 __all__ = [
+    "Headers",
     "Request",
     "Server",
     "StatusCode",
@@ -18,5 +20,4 @@ __all__ = [
     "get_default_headers",
     "get_logger",
     "logger",
-    "serve"
 ]
